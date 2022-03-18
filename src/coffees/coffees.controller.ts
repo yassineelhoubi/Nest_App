ff93129ab9d18df13eb4484a7bd2145d40dc17d7
yassineelhoubi <elhoubiyassine@gmail.com>
2022-03-18 23:42:20 +0100

    dto

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,3 +1,5 @@
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { Body, Controller, Get, Param, Post, Patch, Delete, Query } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 
@@ -17,13 +19,13 @@ export class CoffeesController {
     }
 
     @Post()
-    create(@Body() body) {
-        return this.coffeesService.create(body);
+    create(@Body() createCoffeeDto: CreateCoffeeDto) {
+        return this.coffeesService.create(createCoffeeDto);
     }
 
     @Patch(':id')
-    update(@Param('id') id: string, @Body() body) {
-        return this.coffeesService.update(id, body);
+    update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+        return this.coffeesService.update(id, updateCoffeeDto);
     }
 
     @Delete(':id')
